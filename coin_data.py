5aa26c85ffce63651a5bee8d3d1a680300b76afa
Gosewinckel <gosewinckel.lewis@gmail.com>
2025-03-16 07:28:37 +1100

    Data from binance working

--- coin_data.py
+++ coin_data.py
@@ -1,13 +1,36 @@
 import websocket
 import json
 import time
+import numpy as np
+import matplotlib.pyplot as plt
 
 class coin_data:
     def __init__(self, symbol):
         self.symbol = symbol
         self.data = []
+        self.position_open = False
         self.ws = websocket.WebSocket()
 
     def connect(self):  #connect to websocket
-        ws.connect(f"ws://stream.binance.com:9443/ws/{self.symbol}@aggTrade")
+        self.ws.connect(f"wss://stream.binance.com:9443/ws/{self.symbol}@aggTrade")
+        if self.ws.connected:
+            self.position_open = True
 
+    def price(self):    #return price
+        data = json.loads(self.ws.recv())
+        return float(data["p"])
+
+    def stream(self):   #save live data to data. Must be threaded to avoid problems
+        while self.position_open:
+            data = json.loads(self.ws.recv())
+            self.data.append(float(data['p']))
+            print(self.data)
+            time.sleep(1)
+
+   #TODO
+    def plot_price(self):
+        return
+     
+    def close(self):
+        self.position_open = False
+        self.ws.close()
